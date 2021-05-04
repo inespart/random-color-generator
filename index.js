b78304d6dffed317a3971504682aa73a66a9675f
Ines Part <ines.part91@gmail.com>
2021-05-04 17:41:43 +0200

    Add random color generator

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+console.log('Hello everyone');
+
+const randomColor = require('randomcolor'); // import the script randomcolor
+const chalk = require('chalk'); // import the script chalk
+
+// define user inputs as variables
+const userInputHue = process.argv[2];
+const userInputLuminosity = process.argv[3];
+
+// generate a random color based on user inputs
+const color = randomColor({
+  luminosity: userInputLuminosity,
+  hue: userInputHue,
+});
+
+const str = `
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${color}       #####
+#####                     #####
+###############################
+###############################
+###############################`;
+
+console.log(chalk.hex(color).bold(str));
