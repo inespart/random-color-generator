82ad3b56913ac8bf1df0d5892c98d441d0f5804e
Ines Part <ines.part91@gmail.com>
2021-05-05 12:24:36 +0200

    Remove unnecessary console.logs and space

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('Hello everyone');
-
 const randomColor = require('randomcolor'); // import the script randomcolor
 const chalk = require('chalk'); // import the script chalk
 
@@ -8,6 +6,8 @@ const userInputHue = process.argv[2];
 const userInputLuminosity = process.argv[3];
 
 // generate a random color based on user inputs
+// if user doesn't input values, then the values are undefined and the color is generated randomly
+// supported by library
 const color = randomColor({
   luminosity: userInputLuminosity,
   hue: userInputHue,
